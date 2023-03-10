a1536b35ddf37724e993c5245aaabf84f66cf24e
Julio-Norberto <juliocsscl@gmail.com>
2023-03-10 00:17:07 -0300

    feat: create function FindAll in product_repository_mysql.go

--- internal/infra/repository/product_repository_mysql.go
+++ internal/infra/repository/product_repository_mysql.go
@@ -24,3 +24,26 @@ func (r *ProductRepositoryMysql) Create(product *entity.Product) error {
 
 	return nil
 }
+
+func (r *ProductRepositoryMysql) FindAll() ([]*entity.Product, error) {
+	rows, err := r.DB.Query("select id, name, price from products")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var products []*entity.Product // atribuindo tipo
+	for rows.Next() {
+		var product entity.Product // atribuindo tipo
+		err = rows.Scan(&product.ID, &product.Name, &product.Price)
+
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, &product)
+	}
+	return products, nil
+}
